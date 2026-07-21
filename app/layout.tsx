97e66b5e533f319c5711cfdfaf030f3164b19dbb
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-22 00:39:07 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import "./globals.css";
 import type { Metadata } from "next";
-
 import TelegramProvider from "@/components/TelegramProvider";
 
 export const metadata: Metadata = {
   title: "NOVA",
-  description: "Telegram Mini Apps",
+  description: "Telegram Mini App",
 };
 
 export default function RootLayout({
