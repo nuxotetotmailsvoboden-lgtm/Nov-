d485170500bc670c507025316e3a13bc51b999df
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-21 23:54:06 +0500

    Create page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,28 @@
+export default function DashboardPage() {
+  return (
+    <main
+      style={{
+        color: "#fff",
+        padding: 24,
+      }}
+    >
+      <h1
+        style={{
+          fontSize: 32,
+          fontWeight: 800,
+        }}
+      >
+        Dashboard
+      </h1>
+
+      <p
+        style={{
+          marginTop: 12,
+          color: "#9CA3AF",
+        }}
+      >
+        Добро пожаловать в NOVA.
+      </p>
+    </main>
+  );
+}
