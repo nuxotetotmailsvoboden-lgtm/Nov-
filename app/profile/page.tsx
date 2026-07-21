699e65e422b7d00d859a2343c098350176805351
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-22 00:20:12 +0500

    Create page.tsx

---
+++ app/profile/page.tsx
@@ -0,0 +1,16 @@
+export default function ProfilePage() {
+  return (
+    <main>
+      <h1>Профиль</h1>
+
+      <p
+        style={{
+          marginTop: 20,
+          color: "#A1A1AA",
+        }}
+      >
+        Здесь будут данные пользователя Telegram.
+      </p>
+    </main>
+  );
+}
