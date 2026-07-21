574613a28aec186af1fa5056b38ce8bde995cc72
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-07-22 00:14:09 +0500

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,33 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 interface ButtonProps {
   children: React.ReactNode;
-  onClick?: () => void;
 }
 
-export default function Button({
-  children,
-  onClick,
-}: ButtonProps) {
+export default function Button({ children }: ButtonProps) {
+  const router = useRouter();
+
   return (
     <button
-      onClick={onClick}
+      onClick={() => router.push("/dashboard")}
       style={{
         width: "100%",
         padding: "18px",
         border: "none",
         borderRadius: 18,
         cursor: "pointer",
-
         fontSize: 17,
         fontWeight: 700,
         color: "#fff",
-
         background:
           "linear-gradient(135deg,#7C3AED,#9333EA)",
-
         boxShadow:
-          "0 0 30px rgba(124,58,237,.45)",
-
-        transition: ".25s",
+          "0 0 25px rgba(124,58,237,.45)",
       }}
     >
       {children}
